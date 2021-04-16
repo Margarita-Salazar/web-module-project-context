139899a3165d93d40efa2bc682b30def67771568
unknown <margarita.salazar.mls@gmail.com>
2021-04-16 01:47:45 -0500

    added cartcontext to app, and fixed imports from products component

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import Navigation from './components/Navigation';
 import Products from './components/Products';
 import ShoppingCart from './components/ShoppingCart';
 import ProductContext from './contexts/ProductContext';
+import CartContext from './contexts/CartContext';
 
 function App() {
 	const [products] = useState(data);
@@ -19,18 +20,20 @@ function App() {
 
 	return (
 		<div className="App">
-			<Navigation cart={cart} />
-
-			{/* Routes */}
 			<ProductContext.Provider value={{ products, addItem }}>
-				<Route exact path="/">
-					<Products />
-				</Route>
-			</ProductContext.Provider>
+				<CartContext.Provider value={{cart}}>
+					<Navigation/>
 
-				<Route path="/cart">
-					<ShoppingCart cart={cart} />
-				</Route>
+					{/* Routes */}
+						<Route exact path="/">
+							<Products />
+						</Route>
+
+						<Route path="/cart">
+							<ShoppingCart/>
+						</Route>
+				</CartContext.Provider>
+			</ProductContext.Provider>
 		</div>
 	);
 }
